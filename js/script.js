8a4ea21e910e9d351dc66b95a6e1d7c083ef79a3
Elena Galkina <algalkinaevgen14@gmail.com>
2023-04-19 11:18:24 +0300

    fix: ajust links

---
+++ js/script.js
@@ -0,0 +1,21 @@
+'use strict';
+console.log('hi');
+let admin;
+let name = "John";
+admin = name;
+// alert(admin);
+// let newName = "Иван";
+
+// Вставим переменную
+// alert( `Привет, ${newName}!` );
+// let isGreater = 4 > 1;
+
+// alert( isGreater ); 
+// let isBoss = confirm("Ты здесь главный?");
+
+// alert( isBoss ); 
+// let age = prompt('Сколько тебе лет?', 100);
+
+// alert(`Тебе ${age} лет!`); 
+let oneName = prompt ('What is your name?',"");
+alert(oneName);
